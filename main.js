746f63d7d098766fc72ac1fca7ad69e7c369d192
Graham Hewett <hewettgraham@gmail.com>
2019-02-23 14:06:45 +0000

    starting point for e-commerce product page

---
+++ main.js
@@ -0,0 +1,11 @@
+//create a new Vue Instance: new Vue({OPTIONS})
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    product: "Socks",
+	description: "A pair of warm fuzzy socks",
+	image: "./common/socks-green.jpg",
+	link: "https://www.kodiri.com"
+  }
+});
